495c9701a1669e56f43feeca65a55294e1cddd56
Esneyder <esneyderg357@gmail.com>
2020-04-08 14:52:25 -0400

    primer prueba

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	 app=new Vue({
 	    	el:'#app',
 	    	data:{
-	    		sedes:[],
+	    		sedes:[{nombre:'',direccion:'',telefono_fijo:'',telefono1:'',telefono2:''}],
 	    		s:0,
 	    		programas:[],
+	    		programas_menu:[],
+	    		areas:[],
+	    		categorias:[],
 	    		galeria:[{foto:'e1.jpg',descripcion:'Diplomado en emergencias médicas'},
 	    				{foto:'e2.jpg',descripcion:'Maestría en recursos humanos'},
-	    				{foto:'e3.jpg',descripcion:'Maestría en ecografía'},
 	    				{foto:'e4.jpg',descripcion:'Diplomado en ciencias forenses'},
 	    				{foto:'e5.jpg',descripcion:''},
 	    				{foto:'e6.jpg',descripcion:''},
 	    				{foto:'e7.jpg',descripcion:''},
-	    				{foto:'e8.jpg',descripcion:''},
 	    				{foto:'e9.jpg',descripcion:'Maestria en instrumentación Quirúrgica'},
 	    				{foto:'e10.jpg',descripcion:'Maestría en Derecho Notarial'},
 	    				{foto:'e11.jpg',descripcion:'Maestría en Derecho Procesal Penal'},
@@ -30,7 +31,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	    			}).catch((error)=>{
 	    				//n_error(error);
 	    			}).finally(()=> {
-	    	    		this.initMaps(this.s);
+	    	    		this.getprogramasmenu();
+	    	    		this.initMaps();
 	    			});
 	    		},
 	    		getultimosprogramas: function(){
@@ -48,16 +50,63 @@ document.addEventListener('DOMContentLoaded', function() {
 	    				//global.commit('cargar',false);
 	    			});
 	    		},
-	    		initMaps:function(i){
-	    				/*map=new google.maps.Map(document.getElementById('map'), {
-	        		        center: {lat: this.sedes[i].latitud, lng: this.sedes[i].longitud},
+	    		getprogramasmenu:function(){
+	    			axios.post(SERVER, {
+	    				query:`query{
+								  programas(por_fecha:false,idsede:`+this.sedes[this.s].id+`){
+								    id,postgrado{nombre,categoria{nombre}},version,grupo}
+								}`
+	    			}).then((res)=> {
+	    				datos=res.data.data.programas;
+	    				this.programas_menu=datos;
+	    			}).catch((error)=>{
+	    				//n_error(error);
+	    			}).finally(()=> {
+	    				//global.commit('cargar',false);
+	    				this.activo=true;
+	    			});
+	    		},
+	    		getareas: function(){
+	    			//global.commit('cargar',true);
+	    			axios.post(SERVER,{
+	    				query: `query{areas{id,nombre}}`
+	    			}).then((res)=>{
+	    				lista = res.data.data.areas;
+	    				this.areas = lista;
+	    				this.idarea=lista[0].id;
+	    				this.cargar();
+	    			}).catch((error)=>{
+	    				//n_error(error);
+	    			}).finally(function () {
+	    				//global.commit('cargar',false);
+	    			});
+	    		},
+	    		getcategorias: function(){
+	    			//global.commit('cargar',true);
+	    			axios.post(SERVER,{
+	    				query: `query{categorias{id,nombre}}`
+	    			}).then((res)=>{
+	    				lista = res.data.data.categorias;
+	    				lista.pop();
+	    				this.categorias = lista;
+	    				this.idcategoria=lista[0].id;
+	    			}).catch((error)=>{
+	    				//n_error(error);
+	    			}).finally(()=> {
+	    				this.getareas();
+	    				//global.commit('cargar',false);
+	    			});
+	    		},
+	    		initMaps:function(){
+	    				map=new google.maps.Map(document.getElementById('map'), {
+	        		        center: {lat: this.sedes[this.s].latitud, lng: this.sedes[this.s].longitud},
 	        		        zoom: 16
 		        		});
 		    		    var marker = new google.maps.Marker({
-		    		          position: {lat: this.sedes[i].latitud, lng: this.sedes[i].longitud},
+		    		          position: {lat: this.sedes[this.s].latitud, lng: this.sedes[this.s].longitud},
 		    		          map: map,
 		    		          title: 'ESAM'
-		    		    });*/
+		    		    });
 	    		},
 	    		format:function(date){
 	    			return moment(parseInt(date)).format('DD/MM/YYYY');
@@ -67,7 +116,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	    		ready();
 	    		this.getsedes();
 	    		this.getultimosprogramas();
-	    		//$('.gallery a').simpleLightbox({});
+	    		this.getareas();
+	    		this.getcategorias();
+	    		$('.gallery a').simpleLightbox({});
 	    	}
 	    });
 	  });
